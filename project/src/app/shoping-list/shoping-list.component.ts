17b2d2b231c34ac2c906eb4aaca7de080f51bb63
unknown <pbgaya1993@gmail.com>
2019-01-25 12:28:34 +0530

    build shoping item instucture

--- project/src/app/shoping-list/shoping-list.component.ts
+++ project/src/app/shoping-list/shoping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shaird/ingredient.model';
 
 @Component({
   selector: 'app-shoping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
-
+  ingerdiens: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 7)
+  ];
   constructor() { }
 
   ngOnInit() {
